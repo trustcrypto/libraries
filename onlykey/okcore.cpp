14c809962180b106acd0f5cce50a5cbaa1d18da5
onlykey <root@localhost.localdomain>
2016-08-15 21:56:14 -0400

    Dev vm commit

--- onlykey/okcore.cpp
+++ onlykey/okcore.cpp
@@ -10,26 +10,45 @@
  * modification, are permitted provided that the following conditions are
  * met:
  *
- *    * Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
  *
- *    * Redistributions in binary form must reproduce the above
- *      copyright notice, this list of conditions and the following
- *      disclaimer in the documentation and/or other materials provided
- *      with the distribution.
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *      
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgment:
+ *    "This product includes software developed by the OnlyKey Project
+ *    (http://www.crp.to/ok)"
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 4. The names "OnlyKey" and "OnlyKey Project" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For written permission, please contact
+ *    admin@crp.to.
  *
+ * 5. Products derived from this software may not be called "OnlyKey"
+ *    nor may "OnlyKey" appear in their names without prior written
+ *    permission of the OnlyKey Project.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the OnlyKey Project
+ *    (http://www.crp.to/ok)"
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE OnlyKey PROJECT ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OnlyKey PROJECT OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  *Original U2F Portion
  *Copyright (c) 2015, Yohanes Nugroho
@@ -2088,9 +2107,9 @@ void blink(int times){
   
   int i;
   for(i = 0; i < times; i++){
-    digitalWrite(BLINKPIN, HIGH);
+    analogWrite(BLINKPIN, 255);
     delay(100);
-    digitalWrite(BLINKPIN, LOW);
+    analogWrite(BLINKPIN, 0);
     delay(100);
   }
 }
@@ -2100,7 +2119,7 @@ void fadein(){
           for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
           // sets the value (range from 0 to 255):
           analogWrite(BLINKPIN, fadeValue);
-          delay(15);
+          delay(9);
           }
 }
 
@@ -2109,7 +2128,7 @@ void fadeout(){
           for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
           // sets the value (range from 0 to 255):
           analogWrite(BLINKPIN, fadeValue);
-          delay(15);
+          delay(9);
           }
 }
 
