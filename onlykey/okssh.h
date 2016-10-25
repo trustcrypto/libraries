942f862a892c198c8de114f5ffbef7b1bbaf968f
onlykey <onlykey@users.noreply.github.com>
2016-10-25 15:20:52 -0400

    Update okssh.h

--- onlykey/okssh.h
+++ onlykey/okssh.h
@@ -47,6 +47,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
+ * Thanks to Thomas Sileo for making SSH support possible
  */
 
 #ifdef US_VERSION
