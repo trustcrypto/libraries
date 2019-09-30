d7e1c820cafbe82e9814ba53120595f1131fdcb3
onlykey <onlykey@users.noreply.github.com>
2019-09-30 16:55:47 -0400

    enable for ssh

--- onlykey/okcrypto.cpp
+++ onlykey/okcrypto.cpp
@@ -191,7 +191,7 @@ void GETPUBKEY (uint8_t *buffer) {
 	Serial.println();
 	Serial.println("OKGETPUBKEY MESSAGE RECEIVED");
 	#endif
-	/* Not used yet
+
 	if (buffer[5] < 5 && !buffer[6]) { //Slot 101-132 are for ECC, 1-4 are for RSA
 		if (onlykey_flashget_RSA ((int)buffer[5])) GETRSAPUBKEY(buffer);
 	} else if (buffer[5] < 130 && !buffer[6]) { //132 and 131 and 130 are reserved
@@ -200,7 +200,7 @@ void GETPUBKEY (uint8_t *buffer) {
 	DERIVEKEY(buffer[6], buffer+7);
 	send_transport_response(ecc_public_key, 64, false, false);
 	}
-	*/
+
 }
 
 void DECRYPT (uint8_t *buffer){
