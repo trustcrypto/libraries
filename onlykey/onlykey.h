1daaa987925daa3244c9118581217cd218c20f24
onlykey <onlykey@users.noreply.github.com>
2022-04-16 09:49:18 -0400

    Update onlykey.h

--- onlykey/onlykey.h
+++ onlykey/onlykey.h
@@ -84,7 +84,7 @@
 #define FACTORYKEYS // Attestation key and other keys encrypted using CHIP ID and RNG for unique keys per device
 #define OKversionmaj "3"
 #define OKversionmin "0"
-#define OKversionpat "0"
+#define OKversionpat "1"
 
 #ifndef OKCORE_H
 #include "okcore.h"
